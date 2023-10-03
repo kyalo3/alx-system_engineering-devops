b59e9e12ea3e65a04facf8f6b0b5c7be5e8ca17d
kyalo3 <kyalokimeu0@gmail.com>
2023-10-03 16:40:20 +0300

    0. Simply matching School

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-#a Ruby script that accepts one argument and pass it to a regular expression
-puts ARGV[0].scan(/School$/)
+#a Ruby script that accepts one argument
+puts ARGV[0].scan(/School$/).join
