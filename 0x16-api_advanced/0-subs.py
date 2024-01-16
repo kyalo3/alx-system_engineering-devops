3cb69e50df37c0b494e48c3bf8ae0d67db7a2c1d
Kyalo3 <kyalokimeu0@gmail.com>
2024-01-16 17:35:59 +0300

    pycodestyle

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,24 +1,20 @@
 #!/usr/bin/python3
-# a function that queries the Reddit API
-# Return - 0 on failire, 1 on success
+""" a function that queries the Reddit API """
 import sys
 import requests
 
 
 def number_of_subscribers(subreddit):
+    """a function that queries the Reddit API"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
 
-    headers = {'User-Agent': 'MyBot/1.0'}
+    headers = {'User-Agent': 'MyBot/0.0.1'}
 
     # Make a GET request to the Reddit API
     response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
         data = response.json()
-
         return data['data']['subscribers']
-    elif response.status_code == 404:
-        return 0
     else:
-        print(f"An error: {response.status_code}")
         return 0
