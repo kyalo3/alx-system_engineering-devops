2733fc8f0bf82f9f0bf9a6090bc5007f5fc52fa3
Kyalo3 <kyalokimeu0@gmail.com>
2024-01-16 17:53:59 +0300

    pycodestyle

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """ a function that queries the Reddit API """
+
 import sys
 import requests
 
 
 def number_of_subscribers(subreddit):
     """a function that queries the Reddit API"""
-    headers = {"User-Agent": "my_bot/0.0.1"}
+    headers = {"User-Agent": "MyBot/0.0.1"}
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 
     # Make a GET request to the Reddit API
