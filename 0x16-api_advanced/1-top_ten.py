52ce373e462e518e59c936220809961125300963
Kyalo3 <kyalokimeu0@gmail.com>
2024-01-22 19:33:27 +0300

    untracked files

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
-# a function that queries the Reddit API
-# Return - 0 on failire, 1 on success
+""" a function that queries the Reddit API """
+
 import sys
-from api import number_of_subscribers
+import requests
+
 
 def top_ten(subreddit):
-    try:
-        return number_of_subscribers
-    if subreddit == 0:
-        print("invalid subreddit")
+    """the function prints the titles of 10 hot posts listed"""
+    headers = {"User-Agent": "MyBot/0.0.1"}
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+
+    # Make a GET request to the Reddit API
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
     else:
-        print("valid reddit")
+        return
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-0x0D. Web stack debugging #0
+0x0D. Web stack debugging #1
 DevOps
 SysAdmin
 Scripting
