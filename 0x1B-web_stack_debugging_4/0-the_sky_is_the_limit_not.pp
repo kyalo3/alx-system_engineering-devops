dbcbbb2d71d3b3eff2d3fdaadfd1e7370611b51f
Kyalo3 <kyalokimeu0@gmail.com>
2024-01-26 08:21:53 +0300

    new changes

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,14 +1,11 @@
+#increase the unlimit
 exec { 'fix--for-nginx':
-  # modify the UNLIMIT value
-  command => '/bin/sed -i "s/15/4096" /etc/default/nginx',
-  # specify the path for the sed command
+  command => 'sed -i "s/15/4096" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin',
 }
 
 # restart Nginx
 exec { 'nginx-restart':
-  # Restart Nginx service
-  command => '/etc/init.d/nginx restart',
-  # specify the path for the init.d script
+  command => 'nginx restart',
   path    => '/etc/init.d/',
 }
